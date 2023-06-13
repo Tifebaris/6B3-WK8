db3945d4b2d51a96869be7ed83f19f650c08b46c
tifebaris <boluwatifetemileyi@gmail.com>
2023-06-13 15:58:06 +0100

    feat:Introduction to DOM Manipulation

---
+++ dom.js
@@ -0,0 +1,26 @@
+const heading=document.querySelector('h1')
+
+heading.innerText=`Dora said
+"old men plant trees whose shed they will never
+live to sit under"`
+
+heading.style.color = '#f0f'
+
+const list1 = document.querySelector('li')
+list1.style.background = 'green'
+
+const list=document.querySelectorAll('li')
+list[0].innerText='Frontend web development with Next JS'
+list[1].innerText='Backend development with Django'
+list[1].style.backgroundColor='cyan'
+
+const zero1=document.querySelectorAll('.no-code') [1]
+zero1.style.backgroundColor = 'skyblue'
+zero1.style.padding='3em'
+const zero=document.getElementById('no-code')
+// zero1.style.letterSpacing='20px'
+
+const zeroCode = document.getElementsByClassName('no-code')
+zeroCode[0].style.borderBottom = '10px dashed pink'
+zeroCode[1].style.borderBottom = '10px ridge magenta'
+
---
+++ list.js
@@ -0,0 +1,36 @@
+const li1 = document.querySelector('li:nth-of-type(1)')
+li1.innerText = 'Jide'
+
+const li2 = document.querySelector('li:nth-of-type(3)')
+li2.innerText = 'Tife'
+
+const li3 = document.querySelector('li:nth-of-type(5)')
+li3.innerText = 'Precious'
+
+const list1 = document.getElementsByClassName('left-list')
+list1[0].textContent = 'Joshua'
+list1[1].textContent = 'Sofiat'
+list1[2].textContent = 'Damilare'
+
+
+const ul2 = document.querySelectorAll('li')
+ul2[7].innerText = 'Victor'
+ul2[9].innerText = 'Darasimi'
+ul2[11].innerText = 'Benedict'
+
+const rightUl = document.getElementsByTagName('li')
+rightUl[6].innerText = 'Adetutu'
+rightUl[8].innerText = 'Tayo'
+rightUl[10].innerText = 'Kolawole'
+
+const ul = document.getElementsByClassName('left')
+ul[0].style.backgroundColor = 'pink'
+
+const rightUl2 = document.getElementsByClassName('right')
+rightUl2[0].style.backgroundColor = 'blue'
+
+
+
+
+
+
